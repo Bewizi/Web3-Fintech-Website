b6d4ece763b9a8718e3bb0c6dc681737986b0057
Toluwalase Bewaji <79145995+toluwalase23@users.noreply.github.com>
2023-07-11 00:07:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+/* 
+// Prompt and parseInt
+const name = prompt('Enter username')
+const age = parseInt(prompt('Enter age'))
+let user = {
+  name: "",
+  age: "",
+
+  
+};
+user.sayHi = function () {
+  // alert(this.name), alert(this.age);
+  alert(`${this['name']} ${this['age']}`)
+},
+console.log(name, age);
+user.sayHi();
+*/
+
