ac7d028c4f8ddbb05a3e992591a18d508747ea6a
Bewizi <toluwalase.bewaji@yahoo.com>
2024-02-03 20:39:55 +0100

    bewizi

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
 "use strict";
-/* 
-// Prompt and parseInt
-const name = prompt('Enter username')
-const age = parseInt(prompt('Enter age'))
-let user = {
-  name: "",
-  age: "",
 
-  
-};
-user.sayHi = function () {
-  // alert(this.name), alert(this.age);
-  alert(`${this['name']} ${this['age']}`)
-},
-console.log(name, age);
-user.sayHi();
-*/
 // const dateAndMonth = document.querySelector(".date-month");
 // const specificDate = new Date("2024-01-03");
 // const formattedDate = specificDate.toLocaleDateString("en-US", {
